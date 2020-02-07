f3ec8f17ff904da9a8aff5688617230165565693
misha <misha.teml@gmail.com>
2020-02-07 23:32:56 +0200

    app install and plain basic starctur

---
+++ src/conteiners/MainRoom/MainRoom.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Cntr from "react-bootstrap/Container";
+
+class MainRoom extends Component {
+  render() {
+    return (
+      <div>
+        <header>logo | title | login/singup</header>
+        <Cntr>Catigories | List Of Moves | Fillter</Cntr>
+        <footer>ADS</footer>
+      </div>
+    );
+  }
+}
+export default MainRoom;
