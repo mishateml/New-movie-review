8217fb031ddbaf6f952506dabe879d1334809dcf
misha <misha.teml@gmail.com>
2020-02-23 09:24:00 +0200

    Updates

---
+++ precache-manifest.0d405513d7456ec5349ab7abb220a03e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa8f858538f0154d2220b4662ac58a2",
+    "url": "/New-movie-review/index.html"
+  },
+  {
+    "revision": "1dba68e4e8b3b77adc51",
+    "url": "/New-movie-review/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6cfda84f1402d00303c9",
+    "url": "/New-movie-review/static/css/main.1f1d0e5e.chunk.css"
+  },
+  {
+    "revision": "1dba68e4e8b3b77adc51",
+    "url": "/New-movie-review/static/js/2.6d3d4ee1.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/New-movie-review/static/js/2.6d3d4ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cfda84f1402d00303c9",
+    "url": "/New-movie-review/static/js/main.c1e17b57.chunk.js"
+  },
+  {
+    "revision": "fcb0607467f59c7cda1d",
+    "url": "/New-movie-review/static/js/runtime-main.6600db45.js"
+  },
+  {
+    "revision": "38aea8663d756249c24960d76690d93d",
+    "url": "/New-movie-review/static/media/logo.38aea866.png"
+  }
+]);
